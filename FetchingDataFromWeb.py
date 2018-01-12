ca75e1f9e6a644696cbe9abab96b5b5449fe5be6
Tyler Caldwell <Talon8989@gmail.com>
2018-01-11 21:28:15 -0600

    commit 4

--- FetchingDataFromWeb.py
+++ FetchingDataFromWeb.py
@@ -2,8 +2,11 @@ import requests
 
 import os
 
+#Movie, Currency Exchange Rate, and ZipCode API
+
+#Movie API
+#Referenced the class example
 key = os.environ['omdb_key']
-print (key)
 
 base_url = 'http://www.omdbapi.com/'
 
@@ -12,5 +15,33 @@ movie = input('What Movie Name?')
 params = { 'apikey' : key, 't' : movie }
 data = requests.get(base_url, params).json()
 
-# print(data)
-print(data['Ratings'][0]['Value'])
+print(data['Ratings'][0]['Value']+'\n')
+
+
+#Currency Exchange Rate Api
+#Referenced Slack Channel software_dev_capstone, Scott Kim
+base_url2 = 'https://api.fixer.io/latest?base=USD'
+
+data2 = requests.get(base_url2).json()
+date = data2["date"]
+eur = str(data2["rates"]["EUR"])
+
+print("Exchange Rate on " + date + ": 1 USD to " + eur + " EUR"+'\n')
+
+
+#ZipCode API
+key3 = os.environ['zipcode_key']
+
+zipcode = input('Enter ZipCode: ')
+distance = input('Return Zip Codes within a radius of how many miles: ')
+
+base_url3 = 'https://www.zipcodeapi.com/rest/' + key3 + '/radius.json/' + zipcode +\
+    '/' + distance + 'mile'
+
+data3 = requests.get(base_url3).json()
+
+zipcodes = data3['zip_codes']
+
+for x in zipcodes:
+    print('Zip Code ' + x['zip_code'] + ' is ' + str(x['distance']) + ' miles away')
+
